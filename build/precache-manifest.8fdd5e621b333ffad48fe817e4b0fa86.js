6e95eb399e389808e5f9dadeb6b93d515d9788c0
dineshdevlpr <dineshiaf30@gmail.com>
2021-07-10 08:05:11 +0530

    Updated

---
+++ build/precache-manifest.8fdd5e621b333ffad48fe817e4b0fa86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39165045a4c3f93413591ad9d4db35cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb5c3ddb2d502bc1e8df",
+    "url": "/static/css/main.4e987138.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5c3ddb2d502bc1e8df",
+    "url": "/static/js/main.e293e3c5.chunk.js"
+  },
+  {
+    "revision": "ed72fd0300eba1b1f74d",
+    "url": "/static/js/runtime-main.258720a7.js"
+  }
+]);
